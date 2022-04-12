f72f22e273ec4a44d8af0905fa06a675f9dd7852
MaedotMellion <ethcanada6@gmail.com>
2022-04-12 04:42:51 -0700

    Nat

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ * */
+int main(void)
+{
+	char Maedot[] = "_putchar";
+
+	int c;
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(Maedot[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
