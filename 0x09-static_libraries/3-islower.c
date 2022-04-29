018959f1582b93571de5e8cc58bf3ad327d2c054
MaedotMellion <101402800+MaedotMellion@users.noreply.github.com>
2022-04-30 01:26:03 +0300

    Update 3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,19 +1,10 @@
 #include "main.h"
 /**
- *
- *  * _islower - returns 1 if c is lowercase, 0 otherwise
- *
- *   * @c: integer to be tested
- *
- *    * Return: 1 or 0
- *
- *     */
-
+ * _islower - returns 1 if c is lowercase, 0 otherwise
+ * @c: integer to be tested
+ * Return: 1 or 0
+ */
 int _islower(int c)
-
 {
-
-		return (c >= 97 && c <= 122);
-
+	return (c >= 97 && c <= 122);
 }
-
