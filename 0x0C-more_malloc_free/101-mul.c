8a792dbb655ead3a7928c4d3fb3e29ed6dbd90ec
MaedotMellion <101402800+MaedotMellion@users.noreply.github.com>
2022-05-06 16:26:22 -0700

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -40,7 +40,7 @@ char *create_xarray(int size)
 
 	array = malloc(sizeof(char) * size);
 
-	if (!array)
+	if (array == NULL)
 		exit(98);
 
 	for (index = 0; index < (size - 1); index++)
